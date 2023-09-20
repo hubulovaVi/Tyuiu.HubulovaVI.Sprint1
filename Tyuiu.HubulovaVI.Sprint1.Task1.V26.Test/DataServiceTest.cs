f0f53f16878f683012863b57919b332dbc5e1b59
Tyuiu.HubulovaVI@outlook.com <valeriahb@mail.ru>
2023-09-21 00:33:33 +0500

    Task1

---
+++ Tyuiu.HubulovaVI.Sprint1.Task1.V26.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.HubulovaVI.Sprint1.Task1.V26.Lib;
+
+namespace Tyuiu.HubulovaVI.Sprint1.Task1.V26.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 1.0;
+            double y = 2.0;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(-1, res);
+        }
+    }
+}
